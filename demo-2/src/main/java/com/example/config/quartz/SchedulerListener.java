395614b3163daa3f56c374fed18ecee129e044ba
chenmeikai <562899487@qq.com>
2018-02-06 22:01:39 +0800

    quartz

---
+++ demo-2/src/main/java/com/example/config/quartz/SchedulerListener.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.example.config.quartz;
+
+import org.quartz.SchedulerException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+/**
+ * @author meikai
+ *
+ */
+@Configuration
+public class SchedulerListener  implements ApplicationListener<ContextRefreshedEvent> {
+    @Autowired
+    public MyScheduler myScheduler;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        try {
+            myScheduler.scheduleJobs();
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean(){
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+        return schedulerFactoryBean;
+    }
+
+}
